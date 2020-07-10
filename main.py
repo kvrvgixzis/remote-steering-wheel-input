17f049f2dab91c7b7d7ca17fe85c0ae8d7e84ed0
124 server <splinex@splinex.com>
2020-07-10 17:20:29 +0500

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+from inputs import get_gamepad
+
+
+def main():
+    """Just print out some event infomation when the gamepad is used."""
+    while 1:
+        events = get_gamepad()
+        for event in events:
+            # event.ev_type, event.code, event.state
+
+            # rotate steering wheel
+            if event.code == 'ABS_Z':
+                if event.state > 135 or event.state < 125:
+                    rotate = round(event.state / 255, 2)
+                    print('rotate', rotate)
+
+            # gas
+            if event.code == 'ABS_Y':
+                if event.state > 209:
+                    gas = round((event.state - 208) / 47, 2)
+                    print('gas', gas)
+
+
+if __name__ == "__main__":
+    main()
