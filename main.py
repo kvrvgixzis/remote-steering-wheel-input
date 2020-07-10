e45380d90f0ef07a9166297b58f04b8f3f4e92d4
124 server <splinex@splinex.com>
2020-07-10 17:25:50 +0500

    comments

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
         for event in events:
             # event.ev_type, event.code, event.state
 
-            # rotate steering wheel
+            # rotate steering wheel (0..1)
             if event.code == 'ABS_Z':
                 if event.state > 135 or event.state < 125:
                     rotate = round(event.state / 255, 2)
                     print('rotate', rotate)
 
-            # gas
+            # gas / break (0..1 / -1)
             if event.code == 'ABS_Y':
                 if event.state > 209 or event.state == 0:
                     gas = round((event.state - 208) / 47, 2) if event.state != 0 else -1
