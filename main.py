6e79907f666b764cfc339a57acdad71f321c3f96
124 server <splinex@splinex.com>
2020-07-10 17:53:48 +0500

    reverse

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from inputs import get_gamepad
 
-
 def main():
+    reverse = False
     while 1:
         events = get_gamepad()
         for event in events:
             # event.ev_type, event.code, event.state
 
-            # rotate steering wheel (0..1)
+            # steering
             if event.code == 'ABS_Z':
-                if event.state > 135 or event.state < 125:
-                    rotate = round(event.state / 255, 2)
-                    print('rotate', rotate)
+                steering = event.state
+                print('steering', steering)
 
-            # gas / break (0..1 / -1)
+            if event.code == 'BTN_PINKIE':
+                if event.state == 1:
+                    reverse = True
+                if event.state == 0:
+                    reverse = False
+
+
+            # trottle
             if event.code == 'ABS_Y':
-                if event.state > 209 or event.state == 0:
-                    gas = round((event.state - 208) / 47, 2) if event.state != 0 else -1
-                    print('gas', gas)
-
+                trottle = -event.state if reverse else event.state
+                print('trottle', trottle)
 
 if __name__ == "__main__":
     main()
