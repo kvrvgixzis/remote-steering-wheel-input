2a2727ffa9c1a90d1100b95312eee59208ae2876
124 server <splinex@splinex.com>
2020-07-10 18:49:23 +0500

    wip

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from inputs import get_gamepad
+from numpy import clip
+
+def convert_to_0_255(s):
+    mn = 208
+    mx = 255
+    x = (s - mn) / (mx - mn) * 255
+    x = clip([x], 0, 255)[0]
+    return int(x)
 
 def main():
     reverse = False
     while 1:
         events = get_gamepad()
-        for event in events:
-            # event.ev_type, event.code, event.state
-
+        for event in events: # event.ev_type, event.code, event.state
             # steering
             if event.code == 'ABS_Z':
                 steering = event.state
                 print('steering', steering)
 
+            # check reverse
             if event.code == 'BTN_PINKIE':
-                if event.state == 1:
-                    reverse = True
-                if event.state == 0:
-                    reverse = False
-
+                reverse = event.state
 
             # trottle
             if event.code == 'ABS_Y':
-                trottle = -event.state if reverse else event.state
+                trottle = convert_to_0_255(event.state)
+                trottle = -trottle if reverse else trottle
                 print('trottle', trottle)
+
 
 if __name__ == "__main__":
     main()
